5acd2af664a2017f7f8770ebc13e260540bb57b7
leibaio <leibaio@qq.com>
2024-05-25 23:06:08 +0800

    feat: card light

--- src/hooks/useLightCard.ts
+++ src/hooks/useLightCard.ts
@@ -1,13 +1,31 @@
-import { onMounted, onUnmounted, ref } from "vue"
+import { onMounted, onUnmounted, ref } from 'vue';
 
-export const useLightCard = () => {
-  // 是否展示光
-  const showLight = ref(false)
-  // 获取卡片的 dom 节点
-  const cardRef = ref<HTMLElement | null>(null)
-  let cardOverflow = ''
-  // 获取光的 dom 节点
-  const lightRef = ref<HTMLElement | null>(null)
+interface IOptions {
+  light?: {
+    width?: number; // 宽
+    height?: number; // 高
+    color?: string; // 颜色
+    blur?: number; // filter: blur()
+  };
+}
+
+export const useLightCard = (option: IOptions = {}) => {
+  // 获取卡片的dom节点
+  const cardRef = ref<HTMLDivElement | null>(null);
+  let cardOverflow = '';
+  // 光的dom节点
+  const lightRef = ref<HTMLDivElement>(document.createElement('div'));
+  // 设置光源的样式
+
+  const setLightStyle = () => {
+    const { width = 60, height = 60, color = '#ff4132', blur = 40 } = option.light ?? {};
+    const lightDom = lightRef.value;
+    lightDom.style.position = 'absolute';
+    lightDom.style.width = `${width}px`;
+    lightDom.style.height = `${height}px`;
+    lightDom.style.background = color;
+    lightDom.style.filter = `blur(${blur}px)`;
+  };
 
   // 设置卡片的 overflow 为 hidden
   const setCardOverflowHidden = () => {
@@ -16,69 +34,91 @@ export const useLightCard = () => {
       cardOverflow = cardDom.style.overflow;
       cardDom.style.overflow = 'hidden';
     }
-  }
+  };
   // 还原卡片的 overflow
   const restoreCardOverflow = () => {
     const cardDom = cardRef.value;
     if (cardDom) {
       cardDom.style.overflow = cardOverflow;
     }
-  }
+  };
+
+  // 往卡片添加光源
+  const addLight = () => {
+    const cardDom = cardRef.value;
+    if (cardDom) {
+      cardDom.appendChild(lightRef.value);
+    }
+  };
+  // 删除光源
+  const removeLight = () => {
+    const cardDom = cardRef.value;
+    if (cardDom) {
+      cardDom.removeChild(lightRef.value);
+    }
+  };
+
   // 监听卡片的鼠标移入
   const onMouseEnter = () => {
-    setCardOverflowHidden()
-    showLight.value = true
-  }
+    // 添加光源
+    addLight();
+    setCardOverflowHidden();
+  };
+
+  // use-light-card.ts
+
   // 监听卡片的鼠标移动
   const onMouseMove = (e: MouseEvent) => {
-
     // 获取鼠标的坐标
-    const { clientX, clientY } = e
+    const { clientX, clientY } = e;
     // 让光跟随鼠标
     const cardDom = cardRef.value;
     const lightDom = lightRef.value;
-    if (cardDom && lightDom) {
+    if (cardDom) {
       // 获取卡片相对于窗口的x和y坐标
-      const { x, y } = cardDom.getBoundingClientRect()
+      const { x, y } = cardDom.getBoundingClientRect();
       // 获取光的宽高
-      const { width, height } = lightDom.getBoundingClientRect()
-      lightDom.style.left = `${clientX - x - width / 2}px`
-      lightDom.style.top = `${clientY - y - height / 2}px`
+      const { width, height } = lightDom.getBoundingClientRect();
+      lightDom.style.left = `${clientX - x - width / 2}px`;
+      lightDom.style.top = `${clientY - y - height / 2}px`;
 
-      // // 设置动画效果
-      // const maxXRotation = 10; // x 轴旋转角度
-      // const maxYRotation = 10; // y 轴旋转角度
+      //   设置动画效果
+      const maxXRotation = 10; // X 轴旋转角度
+      const maxYRotation = 10; // Y 轴旋转角度
 
-      // const rangeX = 200 / 2; // x 轴旋转的范围
-      // const rangeY = 200 / 2; // y 轴旋转的范围
+      const rangeX = 200 / 2; // X 轴旋转的范围
+      const rangeY = 200 / 2; // Y 轴旋转的范围
 
-      // const rotateX = ((clientX - x - rangeY) / rangeY) * maxXRotation; // 根据鼠标在 y 轴上的位置计算 x 轴旋转角度
-      // const rotateY = -1 * ((clientY - y - rangeX) / rangeX) * maxYRotation; // 根据鼠标在 y 轴上的位置计算 y 轴旋转角度
+      const rotateX = ((clientX - x - rangeY) / rangeY) * maxXRotation; // 根据鼠标在 Y 轴上的位置计算绕 X 轴的旋转角度
+      const rotateY = -1 * ((clientY - y - rangeX) / rangeX) * maxYRotation; // 根据鼠标在 X 轴上的位置计算绕 Y 轴的旋转角度
 
-      // cardDom.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)` // 设置 3D 透视
+      cardDom.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`; //设置 3D 透视
     }
-  }
-  // 监听卡片的鼠标移出
+  };
+  // 监听卡片鼠标移出
   const onMouseLeave = () => {
-    restoreCardOverflow()
-    showLight.value = false
-  }
+    // 鼠标离开移出光源
+    removeLight();
+    restoreCardOverflow();
+  };
 
   onMounted(() => {
-    cardRef.value?.addEventListener('mouseenter', onMouseEnter)
-    cardRef.value?.addEventListener('mousemove', onMouseMove)
-    cardRef.value?.addEventListener('mouseleave', onMouseLeave)
-  })
+    // 设置光源样式
+    setLightStyle();
+    // 绑定事件
+    cardRef.value?.addEventListener('mouseenter', onMouseEnter);
+    cardRef.value?.addEventListener('mousemove', onMouseMove);
+    cardRef.value?.addEventListener('mouseleave', onMouseLeave);
+  });
 
   onUnmounted(() => {
-    cardRef.value?.removeEventListener('mouseenter', onMouseEnter)
-    cardRef.value?.removeEventListener('mousemove', onMouseMove)
-    cardRef.value?.removeEventListener('mouseleave', onMouseLeave)
-  })
+    // 解绑事件
+    cardRef.value?.removeEventListener('mouseenter', onMouseEnter);
+    cardRef.value?.removeEventListener('mousemove', onMouseMove);
+    cardRef.value?.removeEventListener('mouseleave', onMouseLeave);
+  });
 
   return {
-    showLight,
     cardRef,
-    lightRef
-  }
-}
+  };
+};
--- src/views/mouse-light/index.vue
+++ src/views/mouse-light/index.vue
@@ -1,41 +1,29 @@
 <template>
   <div class="container">
-    <div class="item" ref="cardRef">
-      <div class="light" ref="lightRef" v-if="showLight"></div>
-    </div>
-    <div class="item"></div>
-    <div class="item"></div>
-    <!-- <div class="item" ref="cardRef1">
-      <div class="light" ref="lightRef1" v-if="showLight1"></div>
-    </div>
-    <div class="item" ref="cardRef2">
-      <div class="light" ref="lightRef2" v-if="showLight2"></div>
-    </div>
-    <div class="item" ref="cardRef3">
-      <div class="light" ref="lightRef3" v-if="showLight3"></div>
-    </div> -->
+    <!-- 方块盒子 -->
+    <div class="item" ref="cardRef1"></div>
+    <!-- 方块盒子 -->
+    <div class="item" ref="cardRef2"></div>
+    <!-- 方块盒子 -->
+    <div class="item" ref="cardRef3"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useLightCard } from "@/hooks";
 
-const { cardRef, lightRef, showLight } = useLightCard();
-// const {
-//   cardRef1: cardRef1,
-//   lightRef1: lightRef1,
-//   showLight1: showLight1,
-// } = useLightCard();
-// const {
-//   cardRef2: cardRef2,
-//   lightRef2: lightRef2,
-//   showLight2: showLight2,
-// } = useLightCard();
-// const {
-//   cardRef3: cardRef3,
-//   lightRef3: lightRef3,
-//   showLight3: showLight3,
-// } = useLightCard();
+const { cardRef: cardRef1 } = useLightCard();
+const { cardRef: cardRef2 } = useLightCard({
+  light: {
+    color: "#ffffff",
+    width: 100,
+  },
+});
+const { cardRef: cardRef3 } = useLightCard({
+  light: {
+    color: "yellow",
+  },
+});
 </script>
 
 <style scoped lang="less">
@@ -53,14 +41,6 @@ const { cardRef, lightRef, showLight } = useLightCard();
     height: 125px;
     background: #1c1c1f;
     border: 1px solid rgba(255, 255, 255, 0.1);
-
-    .light {
-      width: 60px;
-      height: 60px;
-      background: #ff4132;
-      filter: blur(40px);
-      position: absolute;
-    }
   }
 }
 </style>
