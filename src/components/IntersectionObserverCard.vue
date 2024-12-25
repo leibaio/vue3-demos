045caa0225ad4798df6c84db4597b00b67dcb57e
weijie.tang <weijie.tang@cootek.com>
2024-12-25 10:40:13 +0800

    feat: IntersectionObserver 尝试

--- src/components/IntersectionObserverCard.vue
+++ src/components/IntersectionObserverCard.vue
@@ -1,10 +1,26 @@
 <template>
-  <h3>IntersectionObserver API</h3>
+  <h3>
+    IntersectionObserver API
+    <a
+      href="https://www.ruanyifeng.com/blog/2016/11/intersectionobserver_api.html"
+      target="_blank"
+      class="text-blue-500 mx-2"
+      >阮一峰教程</a
+    >
+    <a
+      href="https://developer.mozilla.org/zh-CN/docs/Web/API/IntersectionObserver"
+      class="text-blue-500"
+      >MDN</a
+    >
+  </h3>
   <div>
     传统使用 scroll 方法监听，调用目标元素的 getBoundingClientRect()
     方法，再判断是否在视口内。缺点是 scroll 事件密集，计算量大，容易造成性能问题
   </div>
-  <h2 class="font-bold">Enter {{ nowName }}</h2>
+
+  <h2 class="font-bold">
+    Enter <span :style="{ color: nowColor }">{{ nowName }}</span>
+  </h2>
   <div class="w-[500px] h-[500px] overflow-auto border border-blue-500">
     <div v-for="item in cardInfo" :key="item.name">
       <div>{{ item.name }}</div>
@@ -14,7 +30,7 @@
             if (el) cardRefs.push({ el, name: item.name });
           }
         "
-        class="w-full h-[300px]"
+        class="w-full h-[600px]"
         :style="{ backgroundColor: item.color }"
       ></p>
     </div>
@@ -25,6 +41,7 @@
 import { ref, reactive, onMounted, onUnmounted } from "vue";
 
 const nowName = ref("");
+const nowColor = ref("");
 const cardRefs = ref([]);
 
 const cardInfo = reactive([
@@ -40,11 +57,12 @@ onMounted(() => {
   // let io = new IntersectionObserver(callback, option)
   observer = new IntersectionObserver(
     (entries) => {
-      console.log('[ entries ] >', entries)
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           nowName.value = entry.target._name;
-          console.log(`Entered: ${nowName.value}`);
+          nowColor.value = cardInfo.find(
+            (item) => item.name === entry.target._name
+          ).color;
         }
       });
     },
