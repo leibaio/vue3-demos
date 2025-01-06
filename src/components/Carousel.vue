1c2b52a4bbac49f7861489a1a52e0ca77a8475cb
weijie.tang <weijie.tang@cootek.com>
2025-01-06 18:33:38 +0800

    feat: carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -26,19 +26,24 @@
     "
   >
     <div
-      v-for="item in carouselItems"
-      :key="item.id"
-      class="min-w-[300px] h-[200px] mr-[20px] border-solid border-2 border-indigo-600 flex justify-center"
+      class="flex transition-transform duration-0 ease-linear"
+      :style="{ transform: `translateX(${-scrollPosition}px)` }"
     >
-      {{ item.title }}
+      <div
+        v-for="item in displayItems"
+        :key="item.id"
+        class="min-w-[300px] h-[200px] mr-[20px] border-solid border-2 border-indigo-600 flex justify-center"
+      >
+        {{ item.title }}
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 const scrollPosition = ref(0);
-const scrollSpeed = ref(10);
+const scrollSpeed = ref(1);
 const itemWidth = ref(320);
 
 const carouselItems = reactive([
@@ -58,17 +63,22 @@ const carouselItems = reactive([
     id: 4,
     title: "Item 4",
   },
-  {
-    id: 5,
-    title: "Item 5",
-  },
-  {
-    id: 6,
-    title: "Item 6",
-  },
 ]);
 
+const displayItems = computed(() => {
+  return [...carouselItems, ...carouselItems];
+});
 
+const totalWidth = computed(() => {
+  return carouselItems.length * itemWidth.value;
+});
+
+setInterval(() => {
+  scrollPosition.value += scrollSpeed.value;
+  if (scrollPosition.value > totalWidth.value) {
+    scrollPosition.value = 0;
+  }
+}, 10);
 </script>
 
 <style lang="less" scoped></style>
