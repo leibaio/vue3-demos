05d508bf11601a0611c0fa5242d288a3929b88ca
weijie.tang <weijie.tang@cootek.com>
2025-02-12 15:01:37 +0800

    feat: car-icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,23 @@
   <section>
     <div class="flex items-center justify-between p-6">
       <div class="flex items-center space-x-3">
-        <h1
-          class="text-xl font-bold hover:text-blue-200 cursor-pointer"
-          @click="goToAdd"
-        >
-          Add
-        </h1>
-        <h1
-          class="text-xl font-bold hover:text-blue-200 cursor-pointer"
-          @click="goToMouseLight"
-        >
-          Mouse Light!
-        </h1>
+        <div class="car-container">
+          <div class="car-icon"></div>
+          <div class="titles">
+            <h1
+              class="text-l font-bold hover:text-blue-200 cursor-pointer mr-2"
+              @click="goToAdd"
+            >
+              Add
+            </h1>
+            <h1
+              class="text-l font-bold hover:text-blue-200 cursor-pointer"
+              @click="goToMouseLight"
+            >
+              Mouse Light!
+            </h1>
+          </div>
+        </div>
       </div>
       <ThemeSwitch />
       <div>{{ count }}</div>
@@ -51,4 +56,50 @@ onMounted(() => {
   fetchUserInfo();
 });
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.car-container {
+  position: relative;
+  overflow: hidden;
+  width: 40px; // 初始宽度，仅显示图标
+  height: 40px;
+  transition: width 0.3s ease;
+  cursor: pointer;
+  &:hover {
+    width: 300px;
+  }
+  .car-icon {
+    width: 32px;
+    height: 32px;
+    background: url("../assets/car-alt.svg") no-repeat center/cover;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 24px;
+    z-index: 2;
+  }
+  .titles {
+    position: absolute;
+    left: 40px;
+    top: 0;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    white-space: nowrap;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
+  &:hover .titles {
+    opacity: 1;
+  }
+  .title {
+    margin-right: 15px;
+    font-weight: bold;
+    color: #333;
+    transition: color 0.2s ease;
+    &:hover {
+      color: #3498db;
+    }
+  }
+}
+</style>
