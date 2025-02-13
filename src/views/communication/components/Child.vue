57825d549cbdd421ad31eeaca3ada01bd60e9793
weijie.tang <weijie.tang@cootek.com>
2025-02-13 11:03:52 +0800

    feat: emit props

--- src/views/communication/components/Child.vue
+++ src/views/communication/components/Child.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="">Child</div>
+  <div class="">
+    Child
+
+    <n-tooltip trigger="hover">
+      <template #trigger>
+        <n-button @click="handleClickEmit" type="info">childTest</n-button>
+      </template>
+      父组件给子组件传参方法
+    </n-tooltip>
+    {{ props }}
+  </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive } from "vue";
+import { ref, reactive, defineEmits } from "vue";
+
+const props = defineProps(["person"]);
+const emit = defineEmits(["childTest"]);
 
 const childFun = () => {
   console.log("testFun");
+};
+
+const handleClickEmit = () => {
+  emit("childTest", "childTest asd zxc qwe");
 };
 
 // 使用 defineExpose 暴露方法
--- src/views/communication/index.vue
+++ src/views/communication/index.vue
@@ -2,14 +2,31 @@
   <div class="">
     Father
 
-    <Child ref="childRef" />
+    <n-space>
+      <n-tooltip trigger="hover">
+        <template #trigger>
+          <n-button @click="handleClickInvokeChildFun" type="info"
+            >test</n-button
+          >
+        </template>
+        父组件调用子组件方法
+      </n-tooltip>
 
-    <n-tooltip trigger="hover">
-      <template #trigger>
-        <n-button @click="handleClickInvokeChildFun" type="info">test</n-button>
-      </template>
-      父组件调用子组件方法
-    </n-tooltip>
+      <n-tooltip trigger="hover">
+        <template #trigger>
+          <n-button @click="handleClickPassProp" type="info">test1</n-button>
+        </template>
+        父组件给子组件传参方法
+      </n-tooltip>
+    </n-space>
+
+    <n-divider />
+
+    <Child
+      ref="childRef"
+      :person="demoPerson"
+      @child-test="handleClickChildEmit"
+    />
   </div>
 </template>
 <script setup lang="ts">
@@ -20,12 +37,30 @@ interface ChildExpose {
   childFun: () => void;
 }
 
+interface Person {
+  name?: string;
+  age?: number;
+}
+
 const childRef = ref<ChildExpose | null>(null);
+const demoPerson = reactive<Person>({
+  name: "张三",
+  age: 0,
+});
 
 const handleClickInvokeChildFun = () => {
   if (childRef.value) {
     childRef.value.childFun();
   }
+};
+
+const handleClickPassProp = () => {
+  demoPerson.name = "李四";
+  demoPerson.age = Math.random() * 100;
+};
+
+const handleClickChildEmit = (val: any) => {
+  console.log("父组件监听子组件emit事件", val);
 };
 </script>
 <style lang="less" scoped></style>
